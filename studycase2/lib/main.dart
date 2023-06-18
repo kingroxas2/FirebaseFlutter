435f277636279b2a3e42df5838472c8529a612f3
Amir Hamzah <101052053+amoiiir@users.noreply.github.com>
2023-06-18 18:14:18 +0800

    minorChanges, Remove Import

--- studycase2/lib/main.dart
+++ studycase2/lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart'; // Add this import statement
 import 'package:studycase2/Screen/camera.dart';
-import 'package:studycase2/Screen/camera_screen.dart';
 
 List<CameraDescription> cameras = [];
 
