0866f73d87e03e6949c622827ebb1d71437b2388
Firdaus Shahrum <firdausshahrum21@gmail.com>
2023-06-20 01:56:33 +0800

    added text

--- studycase2/lib/main.dart
+++ studycase2/lib/main.dart
@@ -66,7 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Camera App 3000'),
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: Text('Images will display here'),
+        )
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       bottomNavigationBar: BottomAppBar(
         child: Row(
